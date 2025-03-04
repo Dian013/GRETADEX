3d64bd36a1e62e975cad0fde1126b95bd1f6e097
Papourss <mavrodiev.dian@yahoo.fr>
2025-03-04 22:03:24 +0100

    Ptite retouche

--- js/script.js
+++ js/script.js
@@ -1,40 +1,59 @@
 function input_appear() {
-    let bottom_bar_input = document.querySelector("#bottom_bar_input")
-
-    if(bottom_bar_input.style.display == 'block') {
-        bottom_bar_input.style.display = 'none';
+    let bottom_bar_input = document.querySelector("#bottom_bar_input");
+    
+    if (bottom_bar_input.classList.contains("show")) {
+        bottom_bar_input.classList.remove("show");
     } else {
-        bottom_bar_input.style.display  = 'block';
+        bottom_bar_input.classList.add("show");
     }
- 
 }
 
+function filter_pokemon() {
+    let filter_button = document.querySelector("#filter_button");
 
+    console.log("button clicked")
 
-
-
+    if (filter_button.textContent.trim() === 'red') {
+        filter_button.textContent = 'green';
+    } else {
+        filter_button.textContent = 'red';
+    }
+}
 
 fetch("https://pokeapi.co/api/v2/pokemon?limit=151=0")
     .then((res) => res.json())
     .then((data) => {
         const results_list = data.results;
-        const div = document.querySelector('#list_151_first')
 
         for (let i = 0; i < results_list.length; i++) {
 
-            let url_pokemon = results_list.url;
+            let url_pokemon = results_list[i].url;
 
-            // fetch(url_pokemon)              //chercher infos pokemon 1 par 1
-            // .then((res) => res.json())
-            // .then((data) => {
-            //     const results = data.results;
-            //     let pokemon_sticker = document.createElement("div");
-            //     pokemon_sticker.textContent = results_list[i].abilities;
-            // });
+            search_pokemon_sticker(url_pokemon)
+        }
 
-            let pokemon_sticker = document.createElement("div");
-            pokemon_sticker.textContent = results_list[i].name;
+        setTimeout(function(){
+            const div = document.querySelector('#list_151_first')
+            let footer = document.createElement("div");  //permet d'avoir tout les éléments dans le "screen", sans ça une partie sera caché
+            footer.style.height = "130px"
+            footer.textContent = "Tu n'es pas censé voir ceci"
+            div.appendChild(footer);
+        }, 1000);  //set time parce que sinon la case et charger avant les pokemon
+    }
+);
+
+function search_pokemon_sticker(url_pokemon) {
+    fetch(url_pokemon)        
+        .then((res) => res.json())
+        .then((data) => {
+            const sprites = data.sprites;
+            const div = document.querySelector('#list_151_first')
+
+            let pokemon_sticker = document.createElement("img");
+
+            pokemon_sticker.src = sprites.front_default
 
             div.appendChild(pokemon_sticker);
         }
-    });
+    );
+}
