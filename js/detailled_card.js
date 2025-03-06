5305d0a406753d8d773b005155fd7228202b3b9e
Dian <mavrodiev.dian@yahoo.fr>
2025-03-06 15:45:06 +0100

    Responsive

--- js/detailled_card.js
+++ js/detailled_card.js
@@ -1,50 +1,102 @@
+async function search_pokemon_evolutions(){
+    try {     //Trouve 3 evolutions
+        const pokemon_sticker_id = localStorage.getItem("pokemon_sticker_id")
+
+        const data_pokemon_species = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon_sticker_id}`)
+        const element_evolution = await data_pokemon_species.json()  //aller sur le json pokemon species
+        const evolution_chain_url = element_evolution.evolution_chain.url //chercher l'url des evolutions
+
+        const data_evolution_chain = await fetch(`${evolution_chain_url}`) 
+        const element_evolution_chain = await data_evolution_chain.json() //aller sur le json des evolution
+
+        const evolution_1 = element_evolution_chain.chain.species.name
+        const evolution_2 = element_evolution_chain.chain.evolves_to[0].species.name
+        const evolution_3 = element_evolution_chain.chain.evolves_to[0].evolves_to[0].species.name
+
+
+        let tab_evolution = [evolution_1, evolution_2, evolution_3]
+        return tab_evolution
+
+    } catch {
+        try { //Trouve 2 evolutions
+            const pokemon_sticker_id = localStorage.getItem("pokemon_sticker_id")
+    
+            const data_pokemon_species = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon_sticker_id}`)
+            const element_evolution = await data_pokemon_species.json()  //aller sur le json pokemon species
+            const evolution_chain_url = element_evolution.evolution_chain.url //chercher l'url des evolutions
+    
+            const data_evolution_chain = await fetch(`${evolution_chain_url}`) 
+            const element_evolution_chain = await data_evolution_chain.json() //aller sur le json des evolution
+    
+            const evolution_1 = element_evolution_chain.chain.species.name
+            const evolution_2 = element_evolution_chain.chain.evolves_to[0].species.name    
+    
+            let tab_evolution = [evolution_1, evolution_2]
+            return tab_evolution
+
+        }catch{
+            try{ //Trouve pokemon seul
+                const pokemon_sticker_id = localStorage.getItem("pokemon_sticker_id")
+    
+                const data_pokemon_species = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon_sticker_id}`)
+                const element_evolution = await data_pokemon_species.json()  //aller sur le json pokemon species
+                const evolution_chain_url = element_evolution.evolution_chain.url //chercher l'url des evolutions
+        
+                const data_evolution_chain = await fetch(`${evolution_chain_url}`) 
+                const element_evolution_chain = await data_evolution_chain.json() //aller sur le json des evolution
+        
+                const evolution_1 = element_evolution_chain.chain.species.name 
+        
+                let tab_evolution = [evolution_1]
+                return tab_evolution
+            }catch{
+                console.log("Error search evolution")
+            }
+        }
+    }
+}
 
 async function detailled_card_function(){
 //Chercher les infos
-    const pokemon_sticker_id = localStorage.getItem("pokemon_sticker_id");
+    const pokemon_sticker_id = localStorage.getItem("pokemon_sticker_id")
     const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_sticker_id}`)        
     const element = await data.json()
 
     const img = element.sprites.other['official-artwork'].front_default
     const name = element.forms[0].name
-    const stats = element.stats[0].base_stat  //renvoie un nombre entre 0 et 100,   faire une boucle pour parcourir les 5
 
     //Chercher evolution
-    const data_pokemon_species = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon_sticker_id}`)
-    const element_evolution = await data_pokemon_species.json()  //aller sur le json pokemon species
-    const evolution_chain_url = element_evolution.evolution_chain.url //chercher l'url des evolutions
+    search_pokemon_evolutions().then(res => {
+        tab_evolution = res
 
-    const data_evolution_chain = await fetch(`${evolution_chain_url}`) 
-    const element_evolution_chain = await data_evolution_chain.json() //aller sur le json des evolution
+        console.log(tab_evolution)
 
-    try {
-        const evolution_1 = element_evolution_chain.chain.species.name
-        console.log(evolution_1)
-    } catch{
-        console.log("Pas d'évolution disponible")
-    }
+        for (let i = 0; i < tab_evolution.length; i++) {
+            let card_evolution = document.querySelector("card_evolution")
+            let card_evol_stade = document.createElement("div")
 
-    try {
-        const evolution_2 = element_evolution_chain.chain.evolves_to[0].species.name
-        console.log(evolution_2)
-    } catch{
-        console.log("Pas d'évolution disponible")
-    }
-
-    try {
-        const evolution_3 = element_evolution_chain.chain.evolves_to[0].evolves_to[0].species.name
-        console.log(evolution_3)
-    } catch{
-        console.log("Pas d'évolution disponible")
-    }
+            card_evol_stade.textContent = tab_evolution[i]
+            
+            console.log(card_evol_stade)
+            card_evolution.appendChild(card_evol_stade)   //TODO POURQUOI CA NE MARCHE PAS
+        }
+    })     
     //Chercher evolution
 
     //Chercher description
-    const data_characteristic = await fetch(`https://pokeapi.co/api/v2/characteristic/${pokemon_sticker_id}`) 
-    const element_characteristic = await data_characteristic.json() //aller sur le json des evolution
+    try {
+        const data_characteristic = await fetch(`https://pokeapi.co/api/v2/characteristic/${pokemon_sticker_id}`) 
+        const element_characteristic = await data_characteristic.json() //aller sur le json des evolution
 
-    const description = element_characteristic.descriptions[7].description
+        const description = element_characteristic.descriptions[7].description
 
+        const card_description = document.querySelector("#card_description")
+        card_description.textContent = description
+    } catch {
+        const card_description = document.querySelector("#card_description")
+        card_description.textContent = "it doesn't have a description, but all i know is that's my favorite pokemon"
+        card_description.style.textAlign= "center"
+    }
     //Chercher description
 //Chercher les infos
 
@@ -55,9 +107,9 @@ async function detailled_card_function(){
     const card_name = document.querySelector("#card_name")
     card_name.textContent = name
 
-    const card_description = document.querySelector("#card_description")
-    card_description.textContent = description
 
+
+    //TODO faire une fonction pour ici
     const progress_bar_1 = document.querySelector("#progress_bar_1")
     progress_bar_1.value = element.stats[0].base_stat 
     const label_progress_bar_1 = document.querySelector("#label_progress_bar_1")
@@ -87,6 +139,10 @@ async function detailled_card_function(){
     progress_bar_6.value = element.stats[5].base_stat 
     const label_progress_bar_6 = document.querySelector("#label_progress_bar_6")
     label_progress_bar_6.textContent = element.stats[5].stat.name + ` ${element.stats[5].base_stat }%`
+
+
+    //TODO Manque les évolutions, je n'arrive pas à modifier le html dans le try catch, ni extraire la données du try catch
+
 //Intégrer infos à html
 
 }
--- js/script.js
+++ js/script.js
@@ -3,23 +3,23 @@ function home(){
 }
 
 function input_appear() {
-    let bottom_bar_input = document.querySelector("#bottom_bar_input");
+    let bottom_bar_input = document.querySelector("#bottom_bar_input")
     
     if (bottom_bar_input.classList.contains("show")) {
-        bottom_bar_input.classList.remove("show");
+        bottom_bar_input.classList.remove("show")
     } else {
-        bottom_bar_input.classList.add("show");
+        bottom_bar_input.classList.add("show")
     }
 }
 
 function filter_pokemon() {
-    let filter_button = document.querySelector("#filter_button");
+    let filter_button = document.querySelector("#filter_button")
 
     if (filter_button.textContent.trim() === 'Types') {
-        filter_button.textContent = 'Région';
+        filter_button.textContent = 'Région'
         filter_button.style.backgroundColor = '#27DEF2'
     } else {
-        filter_button.textContent = 'Types';
+        filter_button.textContent = 'Types'
         filter_button.style.backgroundColor = '#5EBF76'
     }
 }
@@ -28,11 +28,11 @@ function search_151_pokemon(){
     fetch("https://pokeapi.co/api/v2/pokemon?limit=300=0")
         .then((res) => res.json())
         .then((data) => {
-            const results_list = data.results;
+            const results_list = data.results
 
             for (let i = 0; i < results_list.length; i++) {
 
-                let url_pokemon = results_list[i].url;
+                let url_pokemon = results_list[i].url
 
                 make_pokemon_sticker(url_pokemon)
             }
@@ -50,25 +50,27 @@ function make_pokemon_sticker(url_pokemon) {  //TODO voir comment je peux faire 
             url_species = species.url
             const div = document.querySelector('#list_151_first')
             
-            let pokemon_sticker_div = document.createElement("div");
+            let pokemon_sticker_div = document.createElement("div")
             pokemon_sticker_div.id = "id_div_sticker"
-            let color = await div_background_color(url_species);
-            pokemon_sticker_div.style.border = `2px solid ${color}`;
+            let color = await div_background_color(url_species)
+            pokemon_sticker_div.style.border = `4px solid ${color}`
 
 
 
-            let pokemon_sticker_img = document.createElement("img"); //creation emplacement
-            let pokemon_sticker_name = document.createElement("span");
-            let pokemon_sticker_id = document.createElement("span"); 
-            let pokemon_sticker_types = document.createElement("span");//todo         
-            let pokemon_sticker_see_more = document.createElement("a");           
+            let pokemon_sticker_img = document.createElement("img") //creation emplacement
+            let pokemon_sticker_name = document.createElement("span")
+            let pokemon_sticker_id = document.createElement("span") 
+            let pokemon_sticker_types_div = document.createElement("div") 
+            let pokemon_sticker_see_more = document.createElement("a") 
+
+            pokemon_sticker_name.id = "sticker_span_name"
+            pokemon_sticker_id.id = "sticker_span_id"
             
             
             pokemon_sticker_img.src = sprites.front_default    //chercher données
             pokemon_sticker_name.textContent = forms[0].name
             pokemon_sticker_id.textContent = data.id
-            pokemon_sticker_types//todo
-            pokemon_sticker_see_more.textContent = "Voir Plus"
+            pokemon_sticker_see_more.textContent = "See More"
             
             let pokemon_sticker_id_to_export = data.id
             pokemon_sticker_see_more.addEventListener("click", function(){
@@ -76,10 +78,21 @@ function make_pokemon_sticker(url_pokemon) {  //TODO voir comment je peux faire 
                 localStorage.setItem('pokemon_sticker_id', pokemon_sticker_id_to_export); 
             })
 
-            pokemon_sticker_div.appendChild(pokemon_sticker_img);  //ajouter dans emplacement
-            pokemon_sticker_div.appendChild(pokemon_sticker_name);
-            pokemon_sticker_div.appendChild(pokemon_sticker_id);
-            pokemon_sticker_div.appendChild(pokemon_sticker_see_more);
+
+            const list_types = data.types
+            for (let i = 0; i < list_types.length; i++) {     //Crée un span pour les types du pokemon
+                let pokemon_sticker_type = document.createElement("span") 
+                pokemon_sticker_type.textContent = list_types[i].type.name
+                pokemon_sticker_types_div.appendChild(pokemon_sticker_type)
+            }
+
+
+
+            pokemon_sticker_div.appendChild(pokemon_sticker_img)  //ajouter dans emplacement
+            pokemon_sticker_div.appendChild(pokemon_sticker_name)
+            pokemon_sticker_div.appendChild(pokemon_sticker_id)
+            pokemon_sticker_div.appendChild(pokemon_sticker_types_div)
+            pokemon_sticker_div.appendChild(pokemon_sticker_see_more)
 
 
 
